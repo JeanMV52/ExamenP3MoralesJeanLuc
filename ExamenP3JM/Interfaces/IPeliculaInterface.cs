1b9ab5e4602d661caadd2a15b9cc7d4d7c55579f
JeanMV52 <jeanmorales24@outlook.com>
2025-01-21 08:10:10 -0500

    Creación del modelo de la API de Película, interfaz y lógica de esta.

---
+++ ExamenP3JM/Interfaces/IPeliculaInterface.cs
@@ -0,0 +1,9 @@
+﻿using ExamenP3JM.Models;
+
+namespace ExamenP3JM.Interfaces
+{
+    public interface IPeliculaInterface
+    {
+        public Task<List<Pelicula>> Obtener();
+    }
+}
---
+++ ExamenP3JM/Interfaces/PeliculaInterface.cs
@@ -0,0 +1,21 @@
+﻿using ExamenP3JM.Models;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace ExamenP3JM.Interfaces
+{
+    public class PeliculaInterface : IPeliculaInterface
+    {
+        private string urlApi = "https://freetestapi.com/api/v1/movies?search=inception";
+        public async Task<List<Pelicula>> Obtener()
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync(urlApi);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            JsonNode resultsNode = JsonNode.Parse(responseBody);
+            var peliculasData = JsonSerializer.Deserialize<List<Pelicula>>(resultsNode.ToString());
+            return peliculasData;
+
+        }
+    }
+}
